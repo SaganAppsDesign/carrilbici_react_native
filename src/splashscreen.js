78f540ce1b4b85ea212b63d945c349c49ae7186f
David Arenas <david.arenas@soologic.com>
2021-11-27 21:17:06 +0100

    Mejoras

--- src/splashscreen.js
+++ src/splashscreen.js
@@ -37,13 +37,12 @@ export class SplashScreen extends React.Component {
                               
           <View style={{alignItem:'center', justifyContent:'center', width:wp('100%'), height:hp('100%'), flex:1, flexDirection:'column', paddingTop:hp('75%')}}>  
                         
-                <TextoBienvenida>
+                <TextoBienvenida style={{color:'gray'}}>
                   Una alternativa ecológica...
                 </TextoBienvenida>
                          
           <View style={{alignItems: 'center', flexDirection:'column',flex:9, width:wp('100%'), height:hp('100%')}}>
-
-                      
+                   
                     
          </View>
        
@@ -60,36 +59,3 @@ export class SplashScreen extends React.Component {
 }
 
 
-
-
-const styles = StyleSheet.create({
-
- 
-  logo: {
-    
-    width: "20%",
-    height: "50%",
-    marginBottom: "10%",
-    marginTop: "2%",
-    marginLeft: "59%",
-    marginRight: "0%"
-    
-    
-  },
-
-  texto: {
-    height: hp('8%'),
-    marginBottom:'0%',
-    width:wp('60%'),
-    paddingHorizontal: '0%',
-    backgroundColor: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    borderRadius: 30,
-    paddingLeft: '10%'
-   
-
-  }
-  
-
-});
